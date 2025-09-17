4b469c1118d14bccdfa2d37abe869097c3b7e540
Ethan Ho <omgthereisaleaf@gmail.com>
2025-09-17 19:03:28 +0100

    add room.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
+import 'package:cwscompass/room.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'dart:math' as math;
+import 'dart:io';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
 
 import 'map_canvas.dart';
 
@@ -38,11 +44,22 @@ class _MyHomePageState extends State<MyHomePage> {
   final Rect rect = Offset(50, 50) & Size(150, 150);
   Color color = Color((math.Random().nextDouble() * 0xFFFFFF).toInt()).withOpacity(1.0);
 
-
   void _incrementCounter() {
     setState(() {
       _counter++;
     });
+  }
+
+  Future<Database> openDatabaseFromAssets() async {
+    final directory = await getTemporaryDirectory();
+    final path = join(directory.path, "map.db");
+
+    final data = await rootBundle.load("assets/map.db");
+    List<int> bytes = data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
+
+    await File(path).writeAsBytes(bytes, flush:true);
+
+    return await openDatabase(path);
   }
 
   @override
@@ -61,12 +78,15 @@ class _MyHomePageState extends State<MyHomePage> {
                 painter: MapCanvas(rect, color),
                 size: Size(480, 720),
               ),
-              onTapDown: (TapDownDetails details) {
+              onTapDown: (TapDownDetails details) async {
                 if (rect.contains(details.localPosition)) {
                   setState(() {
                     color = Color((math.Random().nextDouble() * 0xFFFFFF).toInt()).withOpacity(1.0);
                   });
                   print("Rectangle tapped");
+                  final db = await openDatabaseFromAssets();
+                  final room = await Room.fromRoomId(db, 1391733255107376182);
+                  print(room);
                 }
               },
             )
---
+++ lib/room.dart
@@ -0,0 +1,47 @@
+import 'package:sqflite/sqflite.dart';
+
+import 'coordinates.dart';
+
+class Room {
+  final int roomId;
+
+  final int colour;
+  final String subject;
+  final String number;
+  final String? label;
+
+  final List<Coordinates> vertices;
+
+  Room(this.roomId, this.colour, this.subject, this.number, this.label, this.vertices);
+
+  static Future<Room> fromRoomId(Database db, int roomId) async {
+    final roomData = (await db.query("rooms",
+        columns: ["colour", "subject", "number", "label"],
+        where: "room_id = ?",
+        whereArgs: [roomId]
+    ))[0];
+    final label = roomData["label"] as String;
+
+    final vertices = await db.query("room_vertices",
+      columns: ["coordinates"],
+      where: "room = ?",
+      whereArgs: [roomId],
+      orderBy: "sequence"
+    );
+
+    final coordinates = await Future.wait(vertices.map((vertex) async {
+      final result = (await db.query("coordinates",
+        columns: ["latitude", "longitude"],
+        where: "coordinates_id = ?",
+        whereArgs: [vertex["coordinates"] as int]))[0];
+      return Coordinates(result["latitude"] as double, result["longitude"] as double);
+    }));
+
+    return Room(roomId,
+      roomData["colour"] as int,
+      roomData["subject"] as String,
+      roomData["number"] as String,
+      label == "None" ? null : label,
+      coordinates);
+  }
+}
